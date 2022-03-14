9c1b316865c35f0975b96e53c51a8ec4ce2b005c
aaalvinnn <101436409+aaalvinnn@users.noreply.github.com>
2022-03-14 19:26:28 +0800

    1.1
    1、添加了查找记录函数
    2、添加了模糊查找函数
    3、添加了数据库断开语句

--- ms.py
+++ ms.py
@@ -9,18 +9,20 @@ config = {
     'database' : 'Papers'
 }
 con = mysql.connector.connect(**config)
+#创建游标
+mycursor = con.cursor(buffered = True)
+
 #检查表存在函数
 def tableExists(mycursor, name):
     stmt = "SHOW TABLES LIKE '"+name+"'"
     mycursor.execute(stmt)
     return mycursor.fetchone()
+
 #删除表函数
 def dropTable(mycursor, name):
     stmt = "DROP TABLE IF EXISTS" +name
     mycursor.execute(stmt)
-    
-#创建游标
-mycursor = con.cursor(buffered = True)
+
 #增添整条数据
 def add_data(mycursor,val):
     sql = 'INSERT INTO safe1(arXiv,title,authors,date,subjects) VALUES(%s,%s,%s,%s,%s)'
@@ -44,5 +46,27 @@ def search_all_data(mycursor):
     myresult = mycursor.fetchall()
     for x in myresult:
         print(x)
+#查找某条记录（草稿，可以传参）
+def search_data(mycursor):
+    sql = 'SELECT * FROM safe1 WHERE No < 30'
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    for x in myresult:
+        print(x)
+
+#模糊查找(field：字段；val：部分字符)(LIKE函数不区分大小写)
+def fuzzy_check(mycursor,field,val):
+    sql = f"SELECT * FROM safe1 WHERE {field} LIKE '%{val}%'"
+    mycursor.execute(sql)
+    myresult = mycursor.fetchall()
+    if(len(myresult)!=0):
+        for x in myresult:
+            print(x)
+    else:
+        print('无相关结果')
+    
+fuzzy_check(mycursor,'title','computer')
+#断开数据库，释放资源
+con.close()
 
 
